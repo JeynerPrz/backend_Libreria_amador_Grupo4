3055973057eb4d607bf62f863eac66b438bec857
jeynerPrz <jeynerperezalvarez@gmail.com>
2025-09-03 19:46:08 -0600

    Estructura inicial con controladores y rutas creadas

---
+++ app.js
@@ -0,0 +1,60 @@
+
+//Importar las dependencias necesartas 
+import express from 'express'; 
+ import cors from 'cors';
+
+
+//Importar las rutas
+import rutasCategorias from './src/routes/categorias.routes.js';
+import rutasClientes from  './src/routes/clientes.routes.js';
+import rutasCompras from  './src/routes/compras.routes.js';
+import rutasDetaleVentas from  './src/routes/detalle_ventas.routes.js';
+import rutasDetalleCompras from  './src/routes/detalle_compras.routes.js';
+import rutasEmpleados from  './src/routes/empleados.routes.js';
+import rutasProductos from  './src/routes/productos.routes.js';
+
+import rutasVentas from  './src/routes/ventas.routes.js';
+
+
+
+// Crear la aplicación de Express
+const app = express();
+
+// Habilitar CORS para cualquier origen
+app.use(cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-type'],
+}));
+
+
+
+// Middleware para parsear el cuerpo de las solicitudes 
+
+app.use(express.json({ limit: '10mb' })); //10 MB
+
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// Rutas:
+app.use('/api', rutasCategorias);
+app.use('/api', rutasClientes);
+app.use('/api', rutasCompras);
+app.use('/api', rutasDetaleVentas);
+app.use('/api', rutasDetalleCompras);
+app.use('/api', rutasEmpleados);
+app.use('/api', rutasProductos);
+
+app.use('/api', rutasVentas);
+
+
+// Manejo de rutas no encontradas 
+
+app.use((req, res, next) => {
+    
+res.status(404).json({
+message: "La ruta que ha especificado no se encuentra registrada."
+});
+});
+
+
+// Exportar la aplicación export default app;
+export default app;
