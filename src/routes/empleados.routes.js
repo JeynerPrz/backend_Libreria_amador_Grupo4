2650aa2784c7ff585f1e4bdad52192e00473b993
JeynerPrz <jeynerperezalvarez@gmail.com>
2025-10-15 10:43:48 -0600

    correcciones

--- src/routes/empleados.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { obtenerEmpleados } from '../controllers/empleados.controller.js';
-
-const router = Router();
-
-// Ruta para obtener todos los empleados
-router.get('/', obtenerEmpleados);
-
-export default router;
